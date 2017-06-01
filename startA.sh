07f223f7bd2b3c0fe569588e7e3a82a23b66212a
name <email@email.com>
2017-06-01 15:16:22 +0800

    a

--- startA.sh
+++ startA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-iptables -I INPUT -p tcp -m tcp --dport 1025:65535 -j ACCEPT
-iptables -I OUTPUT -p tcp -m tcp --sport 1025:65535 -j ACCEPT
-sudo -u slurm /opt/slurm/sbin/slurmctld -c
 sudo judge/oj_d.py &
+iptables -I INPUT -p tcp -m tcp --dport 1024:65535 -j ACCEPT
+iptables -I OUTPUT -p tcp -m tcp --sport 1024:65535 -j ACCEPT
+sudo -u slurm /opt/slurm/sbin/slurmctld -c
 
