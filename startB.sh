657c3a3a7947b79c399ab0181135b2ea454b1215
root <root@caozj.site>
2017-06-01 15:16:03 +0800

    a

--- startB.sh
+++ startB.sh
@@ -9,5 +9,8 @@ if [ ! -d dat ]; then
 fi
 sudo -u slurm g++ -o judge/judge judge/judge.cpp
 sudo judge/ojroot.py &
+
+iptables -I INPUT -p tcp -m tcp --dport 1024:65535 -j ACCEPT
+iptables -I OUTPUT -p tcp -m tcp --sport 1024:65535 -j ACCEPT
 sudo -u slurm /opt/slurm/sbin/slurmd -c
 
